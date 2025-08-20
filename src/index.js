e9028ff5e46b4538ad1da1648c1a91571ae8a5d0
navi-04 <naveenrajthiyagarajan6@gmail.com>
2025-08-20 21:37:59 +0530

    added the sample deployment file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-//intial creation
+const fs = require('fs');
+const path = require('path');
+
+// Export a function that can be used as a Vercel serverless function
+module.exports = (req, res) => {
+  // Set the content type for the image
+  res.setHeader('Content-Type', 'image/png');
+  
+  // Path to the banner image
+  const imagePath = path.join(__dirname, '../assets/fewinfos-banner.png');
+  
+  try {
+    // Read the image file
+    const imageBuffer = fs.readFileSync(imagePath);
+    
+    // Return the image
+    return res.end(imageBuffer);
+  } catch (error) {
+    console.error('Error serving image:', error);
+    res.statusCode = 500;
+    return res.end('Error loading image');
+  }
+};
